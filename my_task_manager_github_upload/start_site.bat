149e026599d9383b6056c803587361bee5747b2e
Ssimmaa <mdmitrevic@gmail.com>
2026-04-30 10:13:09 +0200

    Delete my_task_manager_github_upload directory

--- my_task_manager_github_upload/start_site.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting TaskMaster at http://127.0.0.1:8000
-echo Keep this window open while you use the site.
-echo Press Ctrl+C in this window to stop the server.
-call ".\scripts\run_server_task.cmd"
--- my_task_manager_github_upload/stop_site.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File ".\site.ps1" stop
-pause
